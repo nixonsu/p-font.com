2e77caff58b685ca269ce80221330b2622715911
ni-xon <nixonuch_@hotmail.com>
2021-11-10 23:05:00 +1100

    Added default template and added rendering for /editor

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('results/', include('results.urls')),
-    path('editor/'), include('editor.urls')
+    path('editor/', include('editor.urls'))
 ]
