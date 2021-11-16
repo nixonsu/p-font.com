abff6f24950ee16f6eb526a1a1610d514eeb9b41
ni-xon <nixonuch_@hotmail.com>
2021-11-16 19:53:07 +1100

    Added math filters as an app

--- config/settings.py
+++ config/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'results',
-    'editor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'results',
+    'editor',
+    'mathfilters'
 ]
 
 MIDDLEWARE = [
