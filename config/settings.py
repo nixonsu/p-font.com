83754dec96eb4aca875448a01d08b235ece61700
ni-xon <nixonuch_@outlook.com>
2022-02-06 21:15:17 +1100

    Switched on debug mode

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&s$dd_85gy_p=$p3c$!rjeqry3zloxeelp1&k1h5_x@x))+555'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
