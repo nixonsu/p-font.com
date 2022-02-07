ccf7ec130f20f6d790f2e277d9b31dff53817290
ni-xon <nixonuch_@outlook.com>
2022-02-08 09:30:23 +1100

    Moved chmod command to postdeploy hook

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -3,7 +3,6 @@
 source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
-sudo chmod a+w db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createfirstsuperuser
